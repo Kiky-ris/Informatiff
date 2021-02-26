928fdc50cdf0211d5e52348a1adee89d9a2951d4
Kilyan Maillard <kefi101218@hotmail.fr>
2021-02-26 05:00:04 +0100

    Ajout component :  home, navbar, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+	{ path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule,
+		RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
