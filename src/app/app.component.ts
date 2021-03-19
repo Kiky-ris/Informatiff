b6b449b2a8c1637a9ed95dc39ca5671153a24982
Kilyan Maillard <kefi101218@hotmail.fr>
2021-03-19 12:35:27 +0100

    ✨feature: Liaison de la base donnée à notre projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import firebase from "firebase/app";
+import "firebase/firestore";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'informatiff';
+
+  constructor() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCQTzVCMa83b4_GHqqfakJeISOyt3vYODc",
+    authDomain: "my-first-app-ffb08.firebaseapp.com",
+    databaseURL: "https://my-first-app-ffb08-default-rtdb.firebaseio.com",
+    projectId: "my-first-app-ffb08",
+    storageBucket: "my-first-app-ffb08.appspot.com",
+    messagingSenderId: "791558001383",
+    appId: "1:791558001383:web:bf294f2d3aa93c72638842",
+    measurementId: "G-E79QCFXR1Y"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  }
+
 }
+
